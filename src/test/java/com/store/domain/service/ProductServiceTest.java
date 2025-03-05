d7f55edf2e112944ba2b2ed72b843f80d2bd035a
vicmag <vmarquez@minsait.com>
2025-03-05 11:11:51 -0600

    Escenario 2. Fase Refactorización Se agrega una nueva prueba

--- src/test/java/com/store/domain/service/ProductServiceTest.java
+++ src/test/java/com/store/domain/service/ProductServiceTest.java
@@ -23,4 +23,21 @@ class ProductServiceTest {
         verify(productRepository).save(product);
         assertEquals(newMinimumStockLevel, product.getMinimumStockLevel());
     }
+
+    @Test
+    void whenSetMinimumStockLevelWithInvalidValue_thenThrowException() {
+        // Arrange
+        ProductRepository productRepository = mock(ProductRepository.class);
+        ProductService productService = new ProductService(productRepository);
+        Product product = new Product("Camiseta Azul", 20);
+        int invalidMinimumStockLevel = 0;
+
+        // Act & Assert
+        IllegalArgumentException exception = assertThrows(
+            IllegalArgumentException.class,
+            () -> productService.setMinimumStockLevel(product, invalidMinimumStockLevel)
+        );
+        assertEquals("El nivel mínimo de stock debe ser mayor que cero.", exception.getMessage());
+        verify(productRepository, never()).save(product);
+    }
 }
