84900e1d9495eee77b8d72a934974faad6e1d484
vicmag <vmarquez@minsait.com>
2025-04-01 17:54:10 +0000

    Escenario 2. Fase Roja, primer paso Definición de la prueba unitaria

--- src/test/java/com/store/domain/service/ProductServiceTest.java
+++ src/test/java/com/store/domain/service/ProductServiceTest.java
@@ -1,24 +1,27 @@
 package com.store.domain.service;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
-
-import com.store.domain.port.ProductRepository; 
-import com.store.domain.model.Product;
+import static org.mockito.Mockito.verify;
 
 public class ProductServiceTest {
+
+    @Test
     void whenSetMinimumStockLevel_thenLevelIsSaved(){
-        // Arrange
+        //Arrage
         ProductRepository productRepository = mock(ProductRepository.class);
         ProductService productService = new ProductService(productRepository);
         Product product = new Product("Camiseta Azul");
-        int newMinimumStockLevel = 15;
+        int newMinimumSotckLevel = 15;
 
-        // Act
-        productService.setMinimumStockLevet(product, newMinimumStockLevel);
+        //Act
+        productService.setMinimumStockLevel(product, newMinimumSotckLevel);
 
-        // Assert        
+        //Assert
         verify(productRepository).save(product);
-        assertEquals(newMinimumStockLevel, product.getMinimumStockLevel());
+        assertEquals(newMinimumSotckLevel,  product.getMinimumStockLevel());
 
     }
+    
 }
