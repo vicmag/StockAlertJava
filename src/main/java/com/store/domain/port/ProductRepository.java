6d35a2fcc6036828a0e4791137e7bda287b85c68
vicmag <vmarquez@minsait.com>
2025-05-12 15:24:27 +0000

    Estructura de archivos para curso comercial de Java

--- src/main/java/com/store/domain/port/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-// ProductRepository.java (Puerto de Dominio)
-package com.store.domain.port;
-
-import com.store.domain.model.Product;
-
-public interface ProductRepository {
-    Product save(Product product);
-    Product findByName(String name);
-}
