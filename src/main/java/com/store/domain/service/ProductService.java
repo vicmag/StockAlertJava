c2fc4d8f0485911ac1b67657c070f89d2cf5eeb2
vicmag <vmarquez@minsait.com>
2025-05-15 16:55:45 +0000

    Escenario 4. Fase Verde

--- src/main/java/com/store/domain/service/ProductService.java
+++ src/main/java/com/store/domain/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.store.domain.service;
 
+import com.store.domain.model.Product;
 import com.store.domain.port.ProductRepository;
 
 public class ProductService {
@@ -10,7 +11,15 @@ public class ProductService {
     }
 
     public void increaseStock(String productName, int increment){
-        // No implementado. Fase Roja
+        // 1. Buscar el producto
+        Product product = productRepository.findByName(productName);
+
+        // 2. Incrementar el stock
+        product.setStock(product.getStock() + increment);
+
+        // 3. Guardar el producto
+        productRepository.save(product);
+
     }
 
 }
