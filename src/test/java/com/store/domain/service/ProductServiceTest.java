1127dbd65b723039f22fb284c264fa3355ee5f5d
vicmag <vmarquez@minsait.com>
2025-05-16 16:33:08 +0000

    Escenario 4 complemento. Fase Roja Flujo de excepción

--- src/test/java/com/store/domain/service/ProductServiceTest.java
+++ src/test/java/com/store/domain/service/ProductServiceTest.java
@@ -1,7 +1,10 @@
 package com.store.domain.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -42,6 +45,34 @@ public class ProductServiceTest {
         verify(productRepository).findByName(productName);
         verify(productRepository).save(product);
         assertEquals(initialStock + increment, product.getStock());
+
+    }
+
+    @Test    
+    void cuandoElStockSeIncrementaDeArticuloNoExistente_entoncesSeDebeActualizarElStockDelArticulo(){
+        // Arrange (Configuración)
+        // 1. Mock
+        ProductRepository productRepository = mock(ProductRepository.class);
+
+        // 2. Servicio con la inyección del mock
+        ProductService productService = new ProductService(productRepository);
+
+        // 3. Producto
+        String productName = "Camiseta";
+        int increment = 5;
+        String mensaje = "Producto inexistente";
+
+        // 4. Comportamiento del mock
+        when(productRepository.findByName(productName)).thenReturn(null);
+
+        // Act (Ejecución) & Assert (Validación)
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+        () -> productService.increaseStock(productName, increment));
+
+        assertEquals(mensaje, exception.getMessage());
+
+        verify(productRepository).findByName(productName);
+        verify(productRepository, never()).save(any());
 
     }
 }
